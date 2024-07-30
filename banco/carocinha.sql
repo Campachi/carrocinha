25b8431abce8081449a24f8c0edc8d11c1599987
Luiz Campachi <luizovc27@gmail.com>
2024-07-30 11:18:18 -0300

    oi

---
+++ banco/carocinha.sql
@@ -0,0 +1,36 @@
+
+CREATE DATABASE carrosinha_control;
+
+
+USE carrosinha_control;
+
+CREATE TABLE cidade (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome_cidade VARCHAR(255) NOT NULL,
+    estado VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE pessoa (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    endereco VARCHAR(255) NOT NULL,
+    bairro VARCHAR(255) NOT NULL,
+    id_cidade INT,
+    cep VARCHAR(10) NOT NULL,
+    FOREIGN KEY (id_cidade) REFERENCES cidade(id)
+);
+
+
+CREATE TABLE animal (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    especie VARCHAR(255) NOT NULL,
+    raca VARCHAR(255),
+    data_nascimento DATE NOT NULL,
+    idade INT NOT NULL,
+    castrado BOOLEAN NOT NULL,
+    id_pessoa INT,
+    FOREIGN KEY (id_pessoa) REFERENCES pessoa(id)
+);
